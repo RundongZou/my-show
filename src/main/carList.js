55fd5fbe0f506f32e97e36064b40141b9876dbfd
zxk <919792828@qq.com>
2017-01-03 21:42:00 +0800

    change

--- src/main/carList.js
+++ src/main/carList.js
@@ -1,14 +1,26 @@
 import React from "react";
 import {Header,Content,Footer} from "./../components/common1"
 import "./../css/carList.css"
+class SubHeader extends React.Component{
+   constructor(props){
+        super(props)
+    } 
+    render(){
+        return <div className="subHeader">
+            
+        </div>
+    }
+}
+
 class CarList extends React.Component{
     constructor(props){
         super(props)
     }
     render(){
         return <div className="page">
-            <Header hasRightBtn={"car"} title="购物车" hasback={false}/>
-            <Content hasFooter={true}/>
+            <Header hasRightBtn={"结算"} title="购物车" hasback={false}/>
+            <SubHeader/>
+            <Content hasFooter={true} hasSubHeader={true}/>
             <Footer hasFooter={true}/>
         </div>
     }
