98ee48b7e7d1d7ccf51ab673ce370ee73f814ed5
zxk <919792828@qq.com>
2017-01-04 09:09:25 +0800

    base css updata

--- src/main/carList.js
+++ src/main/carList.js
@@ -1,13 +1,14 @@
 import React from "react";
 import {Header,Content,Footer} from "./../components/common1"
 import "./../css/carList.css"
+
 class SubHeader extends React.Component{
    constructor(props){
         super(props)
     } 
     render(){
         return <div className="subHeader">
-            
+            <p>商品数量：2 应付总额（不含运费）：<span className="price">￥598</span></p>       
         </div>
     }
 }
@@ -20,7 +21,10 @@ class CarList extends React.Component{
         return <div className="page">
             <Header hasRightBtn={"结算"} title="购物车" hasback={false}/>
             <SubHeader/>
-            <Content hasFooter={true} hasSubHeader={true}/>
+            <Content hasFooter={true} hasSubHeader={true}>
+
+
+            </Content>
             <Footer hasFooter={true}/>
         </div>
     }
