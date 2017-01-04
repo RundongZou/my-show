763821f985b33ffd0d4462292b157452d0e2ad82
zxk <919792828@qq.com>
2017-01-04 17:39:00 +0800

    change name by zk

--- src/App/app.js
+++ src/App/app.js
@@ -2,18 +2,18 @@ import {Header,Content,Footer} from "./../components/common1"
 import React from "react";
 import ReactDOM from "react-dom"
 import { Router, Route, IndexRoute, Link, IndexLink,hashHistory } from 'react-router'
-import {IndexPage} from "./../page/components/indexPage"
-import Pro_list from "./../page/components/pro_list"
-import {ListPage} from "./../page/components/listPage"
-import {Detail} from "./../page/components/detailPage"
+import {IndexPage} from "./../product/components/indexPage"
+import Pro_list from "./../product/components/pro_list"
+import {ListPage} from "./../product/components/listPage"
+import {Detail} from "./../product/components/detailPage"
 
 //user
 import Login from "./../user/components/login"
 import Reg from "./../user/components/Reg"
 import My from "./../user/components/my"
-//dingdan
-import CarList from "./../dingdan/component/carList"
-import More from "./../dingdan/component/more"
+//order
+import CarList from "./../order/component/carList"
+import More from "./../order/component/more"
 class Page extends React.Component{
     constructor(props){
         super(props)
