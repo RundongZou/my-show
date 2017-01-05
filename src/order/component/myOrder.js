e7389a0a458ee0219edcebbe39a39e777b4eb8ed
zxk <919792828@qq.com>
2017-01-05 17:40:05 +0800

    pay by zk

--- src/order/component/myOrder.js
+++ src/order/component/myOrder.js
@@ -5,30 +5,7 @@ import ReactIScroll from "react-iscroll";
 import {options} from "./../../config/config"
 import "./../css/myOrder.css"
 import OrderList from "./../../components/orderList"
-var userID=localStorage["userID"];
-var data=JSON.parse(localStorage.getItem("orderData")||"[]")
-var orderData={
-    "0":[],
-    "1":[],
-    "2":[],
-    "3":[],
-    "4":[],
-    "totalNum":0,
-    "totalPrice":0
-}
-for(var i=0;i<data.length;i++){
-    orderData["0"]=orderData["0"].concat(data[i].orderProductData)
-    orderData["totalNum"]+=data[i].totalNum*1;
-    orderData["totalPrice"]+=data[i].totalPrice*1
-    if(data[i].orderState==1){
-        orderData["1"]=orderData["1"].concat(data[i].orderProductData)
-    }else if(data[i].orderState==2){
-        orderData["2"]=orderData["2"].concat(data[i].orderProductData)
-    }else if(data[i].orderState==3){
-        orderData["3"]=orderData["3"].concat(data[i].orderProductData)
-    }
-}
-console.log(orderData)
+
 //二级头部模块
 // Pending payment待付款  Pay Now 立即付款  cancel order取消订单
 class Action{
@@ -48,12 +25,47 @@ class SubHeader extends React.Component{
         </ul>
     }
 }
+class OrderC extends React.Component{
+    constructor(props){
+        super(props)
+    }
+    render(){
+        if(this.props.num<=1){
+            return <div>
+                
+            </div>
+        }
+    }
+}
 class MyOrder extends React.Component{
     constructor(props){
         super(props);
+        var userID=localStorage["userID"];
+        var data=JSON.parse(localStorage.getItem("orderData")||"[]")
+        this.orderData={
+            "0":[],
+            "1":[],
+            "2":[],
+            "3":[],
+            "4":[],
+            "totalNum":0,
+            "totalPrice":0
+        }
+        for(var i=0;i<data.length;i++){
+            this.orderData["0"]=this.orderData["0"].concat(data[i].orderProductData)
+            this.orderData["totalNum"]+=data[i].totalNum*1;
+            this.orderData["totalPrice"]+=data[i].totalPrice*1
+            if(data[i].orderState==1){
+                this.orderData["1"]=this.orderData["1"].concat(data[i].orderProductData)
+            }else if(data[i].orderState==2){
+                this.orderData["2"]=this.orderData["2"].concat(data[i].orderProductData)
+            }else if(data[i].orderState==3){
+                this.orderData["3"]=this.orderData["3"].concat(data[i].orderProductData)
+            }
+        }
         var _this=this;
         this.state={
-            "data":orderData,
+            "data":this.orderData,
             "num":0
         }
         Action.click=function(index){
@@ -62,13 +74,24 @@ class MyOrder extends React.Component{
             })
         }
     }
-    cancelOrder(){
-        console.log("取消订单");
+    cancelOrder(num){
         localStorage.setItem("orderData","[]");
+        this.orderData[num]=[];
+        this.setState({
+            "data":this.orderData
+        })
+    }
+    payNow(num){
+        console.log(num);
+        var arr=this.orderData[1];
+        this.orderData[1]=[];
+        this.orderData[2]=arr;
+        this.setState({
+            "data":this.orderData
+        })
     }
     render(){
         var data=this.state.data[this.state.num];
-        console.log(this.state.num)
         if(data.length<1){
             return <div className="page" id="myOrder-page">
                 <Header  title="我的订单" hasback={false}/>
@@ -88,7 +111,9 @@ class MyOrder extends React.Component{
                                 <p>共<span className="active">{this.state.data.totalNum}</span>件 商品实付<span  className="active">￥{this.state.data.totalPrice.toFixed(1)}</span></p>
                             </div>
                             <div className="orderBox">
-                                <span>待付款</span><button>立即付款</button><button onClick={()=>this.cancelOrder()}>取消订单</button>
+                                <span>待付款</span>
+                                <button onClick={()=>this.payNow(this.state.num)}>立即付款</button>
+                                <button onClick={()=>this.cancelOrder(this.state.num)}>取消订单</button>
                             </div>
                         </OrderList>
                     </ReactIScroll>   
